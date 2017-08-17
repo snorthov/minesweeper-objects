d6aed9f87a26f91ce6cb1e9ccfdb4c8194c9ce82
STEPHEN NORTHOVER <snorthov@ca.ibm.com>
2017-08-17 14:35:48 +0000

    simplify listening on a port

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 /*eslint-env node, express */
 
 // Initialize Express
-var express = require('express');
+var express = require("express");
 var app = express();
  
 // Set up a simple static server for the public directory
 app.use('/', express.static(__dirname + "/public"));
 
-// Get IP of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application
-var host = process.env.VCAP_APP_HOST || 'localhost';
-
-// Get the port on the DEA for communication with the application
-var port = process.env.VCAP_APP_PORT || 3000;
-
-// Serve the application
-//TODO - print when the server is ready
-app.listen(port, host);
+// Listen for requests on a prort
+var port = process.env.VCAP_APP_PORT || process.env.PORT || process.env.port || 3000;
+app.listen(port, function() {
+	console.log("Server running on port: %d", port);
+});
