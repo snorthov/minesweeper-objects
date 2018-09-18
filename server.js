6e09bfe8de3c9620964d5e3a48053b0c69e07b41
STEPHEN NORTHOVER <snorthov@ca.ibm.com>
2018-09-18 13:18:37 +0000

    fix spelling error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 // Set up a simple static server for the public directory
 app.use('/', express.static(__dirname + "/public"));
 
-// Listen for requests on a prort
+// Listen for requests on a port
 var port = process.env.VCAP_APP_PORT || process.env.PORT || process.env.port || 8081;
 app.listen(port, function() {
 	console.log("Server running on port: %d", port);
