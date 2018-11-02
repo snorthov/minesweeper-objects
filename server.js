1b5c16b2c54eade98cf9c7c225a6855aab23775f
snorthov <snorthov@ca.ibm.com>
2018-11-02 13:08:10 -0400

    accept the port as a command line arg

--- server.js
+++ server.js
@@ -3,12 +3,12 @@
 // Initialize Express
 var express = require("express");
 var app = express();
- 
+
 // Set up a simple static server for the public directory
 app.use('/', express.static(__dirname + "/public"));
 
 // Listen for requests on a port
-var port = process.env.VCAP_APP_PORT || process.env.PORT || process.env.port || 8081;
+var port = process.argv[2] || process.env.PORT || process.env.port || process.env.VCAP_APP_PORT || 8081;
 app.listen(port, function() {
 	console.log("Server running on port: %d", port);
 });
